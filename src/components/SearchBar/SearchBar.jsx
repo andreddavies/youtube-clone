f82aec3d537db3656a3dd2bfe945aa7c1a9257bf
Andre <andreddavies@hotmail.com>
2021-10-15 12:29:25 -0300

    Adding search input value catch function

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,23 @@
+import './SearchBar.css';
 import React from 'react';
 
 const SearchBar = props => {
+
+    const searchAbout = e => {
+        
+        if(e.keyCode === 13) {
+            const about = e.target.value;
+            console.log(about);
+        }
+    };
+
     return (
         <div className="search-bar">
-            <input type="text" />
+            <input 
+                type="text" 
+                placeholder="Search..." 
+                onKeyDown={(e) => searchAbout(e)}
+            />
         </div>
     );
 };
