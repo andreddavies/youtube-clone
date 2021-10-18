daf238a10aa9b5eb643df7152d1181b745038286
Andre <andreddavies@hotmail.com>
2021-10-18 15:10:27 -0300

    Implementing store response and styling

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,12 +1,52 @@
 import './VideoList.css';
-import React from "react";
+import React, { useState } from "react";
+import {connect} from 'react-redux'
 
 const VideoList = props => {
+    
+    const videoItem = (video) => {
+        return (
+            <li className="list-item" key={video.id.videoId}>
+                <div className="image-wrapper">
+                    <img src={video.snippet.thumbnails.default.url} alt="imagem-do-vídeo" />
+                </div>
+                <div className="item-content">
+                    <h4 className="content-header">{video.snippet.title}</h4>
+                    <p>{video.snippet.description}</p>
+                </div>
+            </li>
+        )
+    };
+    
     return (
         <div className="video-list">
-            <h1>Video List</h1>
+            <ul className="list">
+                {
+                    props.loading && (
+                        <div className="loader-background">
+                            <div className="loader-wrapper">
+                                <div className="loader"></div>
+                                <div className="title">Carregando...</div>
+                            </div>
+                        </div>
+                    )
+                }
+                {
+                    props.videos.map(video => {
+                        return videoItem(video);
+                    })
+                }
+            </ul>
         </div>
     )
 };
 
-export default VideoList;
+const mapStateToProps = (state) => {
+    return {
+        videos: state.search.videos,
+        loading: state.search.loading,
+        error: state.search.error,
+    }
+};
+
+export default connect(mapStateToProps, null)(VideoList);
