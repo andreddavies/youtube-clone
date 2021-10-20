70e4c9a11ffd3f25250a6fc37c3c06a90846d2e7
Andre <andreddavies@hotmail.com>
2021-10-20 12:39:43 -0300

    Connecting reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import search from "./searchReducers";
+import player from "./playerReducers";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   search,
+  player,
 });
 
 export default rootReducer;
