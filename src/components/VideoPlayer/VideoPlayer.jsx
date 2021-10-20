e8753f4b8766a41cc8a6657c4094d0477c66a59a
Andre <andreddavies@hotmail.com>
2021-10-20 16:09:29 -0300

    Connecting component, implementing video player and styling

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 import './VideoPlayer.css';
+import { connect } from 'react-redux'
 
 const VideoPlayer = props => {
+
+    
     return (
         <div className="video-player">
-            <h1>Video Player</h1>
+            <div className="player-container">
+                {
+                    props.video.id ? 
+                        <iframe 
+                            width="100%" 
+                            height="100%" 
+                            src={"https://www.youtube.com/embed/" + props.video.id.videoId}
+                            title="YouTube video player" 
+                            frameborder="0" 
+                            allow="
+                                autoplay; 
+                                gyroscope; 
+                                accelerometer; 
+                                clipboard-write; 
+                                encrypted-media; 
+                                picture-in-picture
+                            " 
+                            allowfullscreen
+                        >
+                        </iframe> : 
+                        <h3>Escolha um vídeo para continuar...</h3>
+                    }
+            </div>
+            {
+                props.video.id && (
+                    <div className="description">
+                        <h2 className="title">{props.video.snippet.title}</h2>
+                        <p>{props.video.snippet.description}</p>
+                    </div>
+                )
+            }
         </div>
     );
 };
 
-export default VideoPlayer;
+const mapStateToProps = state => {
+    return {
+        video: state.player.video
+    }
+};
+
+export default connect(mapStateToProps, null)(VideoPlayer);
