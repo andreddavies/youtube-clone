a70c095284cf4a6974da6f2dbca2d50c89c54d45
Andre <andreddavies@hotmail.com>
2021-10-20 16:04:09 -0300

    Creating an adding Header and Footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="contact"><a href="https://www.linkedin.com/in/andre-davi-davies-24a69b17a/">Linkedin</a></div>
+            <p>© Todos os direitos reservados.</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+                <h2>CLONETUBE</h2>
+        </header>
+    );
+};
+
+export default Header;
